ae5ba20bbfbb0d46e2bca9043ce6685685addc9e
Dave Humphrey <dave@uesp.net>
2025-05-23 15:08:49 -0400

    add goggle tag manager

--- includes/SiteCustomCodeHooks.php
+++ includes/SiteCustomCodeHooks.php
@@ -314,6 +314,7 @@ $extra";
 		self::SetupUespFavIcons($out);
 		self::SetupUespLongitudeAds($out);
 		self::SetupUespTwitchEmbed($out);
+		self::SetupUespGoogleTags($out);
 		$out->addModules('ext.UespCustomCode.search.suggest');
 		return true;
 	}
@@ -344,6 +345,25 @@ $extra";
 		$out->addLink(array('rel' => 'icon', 'type' => 'image/png', 'href' => 'https://images.uesp.net/favicon-96.png',  'sizes' => '96x96'));
 		$out->addLink(array('rel' => 'icon', 'type' => 'image/png', 'href' => 'https://images.uesp.net/favicon-128.png', 'sizes' => '128x128'));
 		$out->addLink(array('rel' => 'icon', 'type' => 'image/png', 'href' => 'https://images.uesp.net/favicon-256.png', 'sizes' => '256x256'));
+	}
+	
+	
+	private static function SetupUespGoogleTags(&$out)
+	{
+		if (self::UESP_isShowAds()) 
+		{
+			$out->addHeadItem("uespGoogleTag", "<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T7R6MXMM');</script>
+<!-- End Google Tag Manager -->");
+			$out->prependHTML("<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-T7R6MXMM'
+height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->");
+		}
 	}
 
 	private static function SetupUespLongitudeAds(&$out)
